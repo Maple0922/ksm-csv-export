7b81e332d0d7633f5b582d88029425218ee9626c
Futo Nakajima <maple0922sept@gmail.com>
2021-07-13 15:04:39 +0900

    Update: CSVImport

--- app/Imports/CSVImport.php
+++ app/Imports/CSVImport.php
@@ -4,14 +4,33 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Facades\Excel;
 
 class CSVImport implements ToCollection
 {
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     public function collection(Collection $collection)
     {
-        //
+        $importFile = fopen($this->filePath, "r");
+
+        $columns = [
+            'Number',
+            'Area',
+            'XM',
+            'YM',
+            'Major',
+            'Minor',
+            'Angle',
+            'Slice'
+        ];
+
+        return fgetcsv($importFile);
     }
 }
